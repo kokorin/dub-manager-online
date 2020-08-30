2f2f30be4b807385cd330d4a23673cfca1955b11
Denis Kokorin <kokorin86@gmal.com>
2020-08-30 10:14:58 +0300

    JaxbConverterFactory

--- server/src/main/java/dmo/server/integration/anidb/AnidbConfig.java
+++ server/src/main/java/dmo/server/integration/anidb/AnidbConfig.java
@@ -1,15 +1,13 @@
 package dmo.server.integration.anidb;
 
 import dmo.server.util.okhttp.XmlUngzipInterceptor;
+import dmo.server.util.retrofit.JaxbConverterFactory;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
-import retrofit2.converter.jaxb.JaxbConverterFactory;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,18 +23,15 @@ public class AnidbConfig {
         OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
         interceptors().forEach(clientBuilder::addInterceptor);
 
-        JAXBContext jaxbContext;
-        try {
-            jaxbContext = JAXBContext.newInstance(AnidbAnimeLight.class, AnidbAnime.class, AnidbError.class);
-        } catch (JAXBException e) {
-            throw new RuntimeException("Failed to create JAXB context");
-        }
-
         return new Retrofit.Builder()
                 //because anidb uses different hosts for anime list and anime
                 .baseUrl("http://localhost/why/")
                 .client(clientBuilder.build())
-                .addConverterFactory(JaxbConverterFactory.create(jaxbContext))
+                .addConverterFactory(JaxbConverterFactory.create(
+                        AnidbAnime.class,
+                        AnidbAnimeLightList.class,
+                        AnidbError.class
+                ))
                 .build();
     }
 
--- server/src/main/java/dmo/server/util/retrofit/JaxbConverterFactory.java
+++ server/src/main/java/dmo/server/util/retrofit/JaxbConverterFactory.java
@@ -7,7 +7,13 @@ import retrofit2.Retrofit;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
@@ -15,7 +21,7 @@ import java.lang.reflect.Type;
 public class JaxbConverterFactory extends Converter.Factory {
     private final JAXBContext jaxbContext;
 
-    public static JaxbConverterFactory create(Class<?> ...types) {
+    public static JaxbConverterFactory create(Class<?>... types) {
         JAXBContext jaxbContext;
         try {
             jaxbContext = JAXBContext.newInstance(types);
@@ -34,7 +40,43 @@ public class JaxbConverterFactory extends Converter.Factory {
         return null;
     }
 
-    public static class ResponseConverter extends Converter<ResponseBody, ?> {
+    public static class ResponseConverter<T> implements Converter<ResponseBody, T> {
+        private final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        private final JAXBContext jaxbContext;
+        private final Class<T> type;
 
+        public ResponseConverter(JAXBContext jaxbContext, Class<T> type) {
+            this.jaxbContext = jaxbContext;
+            this.type = type;
+
+            // Prevent XML External Entity attacks (XXE).
+            xmlInputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+            xmlInputFactory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+        }
+
+        @Override
+        public T convert(ResponseBody responseBody) throws IOException {
+            Object parsed;
+
+            try (Reader reader = responseBody.charStream()) {
+                Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+                XMLStreamReader streamReader = xmlInputFactory.createXMLStreamReader(reader);
+                parsed = unmarshaller.unmarshal(streamReader);
+            } catch (JAXBException | XMLStreamException e) {
+                throw new RuntimeException("Failed to parse response", e);
+            }
+
+            if (parsed == null) {
+                return null;
+            }
+
+            if (type.isAssignableFrom(parsed.getClass())) {
+                @SuppressWarnings("unchecked")
+                T result = (T) parsed;
+                return result;
+            }
+
+            throw new RuntimeException("Can't convert " + parsed + " to " + type);
+        }
     }
 }
