85f291fe82ed2fd4f79d51c394b93df76646da1f
Denis Kokorin <kokorin86@gmail.com>
2020-09-08 19:10:31 +0300

    WIP: react.js routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
-import AnimeViewContainer from "./component/AnimeView";
+import {Route, Switch} from "react-router-dom";
+import AnimeTable from "./component/AnimeTable";
+import AnimeView from "./component/AnimeView";
 
+interface AnimeViewRouteProps {
+    match: {
+        params: {
+            animeId:number
+        }
+    }
+}
+
+class AnimeViewRoute extends React.Component<AnimeViewRouteProps, any> {
+
+    render() {
+        return (
+            <AnimeView animeId={this.props.match.params.animeId} />
+        );
+    }
+}
 
 class App extends React.Component {
     render() {
         return (
-            <AnimeViewContainer animeId={1}/>
+            <main>
+                <Switch>
+                    <Route path="/" component={AnimeTable} exact/>
+                    <Route path="/anime/:animeId" component={AnimeViewRoute}/>
+                </Switch>
+            </main>
+            /*<AnimeViewContainer animeId={1}/>*/
         );
     }
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,11 +10,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 // components takes precedence over default styles.
 
 ReactDOM.render(
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>*/
-    <App/>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
